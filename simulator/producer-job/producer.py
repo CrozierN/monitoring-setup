f57092995e9fec29f787ed6efb36a7532d12ee57
CrozierN <crozierntwe@gmail.com>
2024-04-04 17:48:09 +0200

    feat: add producer job

--- simulator/producer-job/producer.py
+++ simulator/producer-job/producer.py
@@ -4,6 +4,8 @@ from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.propagate import inject
+import requests
 
 service_name = "producer-service"
 
@@ -12,10 +14,18 @@ resource = Resource(attributes={
 })
 
 trace.set_tracer_provider(TracerProvider(resource=resource))
-
 otlp_exporter = OTLPSpanExporter(endpoint=f'{os.getenv("OTLP_ENDPOINT", "http://localhost:56290")}/v1/traces')
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
 tracer = trace.get_tracer(__name__)
 
-with tracer.start_as_current_span("example-span"):
-    print("Hello, trace!")
+def send_message_to_broker(message):
+    with tracer.start_as_current_span("produce_message"):
+        headers = {}
+        inject(headers)
+        response = requests.post(f'{os.getenv("BROKER_ENDPOINT","http://localhost:5000")}/broker', json=message, headers=headers)
+        return response.text
+    
+if __name__ == "__main__":
+    message = {"data": "Hello, Consumer!"}
+    print("Producer sending message to broker:", message)
+    send_message_to_broker(message)
